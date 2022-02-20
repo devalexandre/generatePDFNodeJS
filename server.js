e6fa82d8ee7bfdcc42009127bba7e3eecc2ada95
devalexandre <alexandre@indev.net.br>
2022-02-20 13:51:40 -0300

    start

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const polka = require('polka');
 const { json } = require("@polka/parse");
 const pdf = require('html-pdf')
 
+const PORT = process.env.PORT || 3000;
+
 polka()
 .use(json())
 .get("/", (req, res) => {
@@ -24,6 +26,6 @@ polka()
     })
 
 })
-.listen(3000,()=>{
+.listen(PORT,()=>{
     console.log("listening on port 3000");
 })
